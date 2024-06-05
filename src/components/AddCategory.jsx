fc151103e492fc1f5a8489569315850345218235
Bryan <rchongcervantes@gmail.com>
2024-06-05 23:58:03 +0200

    preventDefault: Crear un formuario y conservar el valor de input al hacer 'Enter' en el input del formulario.

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -3,16 +3,21 @@ import {useState} from "react";
 export const AddCategory = () => {
 
     const [ inputValue, setInputValue ] = useState('One Punch');
-    const onInputChange = ( target ) => {
+
+    const onInputChange = ({ target }) => {
         setInputValue( target.value )
+    }
+    const OnSubmit = (event) => {
+        event.preventDefault()
+        console.log(inputValue)
     }
 
     return(
-        <form>
+        <form onSubmit={ OnSubmit }>
             <input
                 type="text"
                 placeholder="Buscar gif"
-                value={inputValue}
+                value={ inputValue }
                 onChange={ onInputChange }
             />
         </form>
