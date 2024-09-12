4f3c40a6878cc14896ff3ffd6208beb139168f14
Bryan <rchongcervantes@gmail.com>
2024-09-12 23:06:02 +0200

    Obtener las imagenes y printar su contenido.

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -3,19 +3,27 @@ import { getGifs } from "../helpers/getGifs.js";
 
 export const GifGrid = ({ category }) => {
 
-    const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState([]);
 
+    const getImages = async() => {
+        const newImages = await getGifs( category );
+        setImages(newImages);
+    }
 
     useEffect(() => {
-        getGifs(category);
-
+        getImages();
     }, [])
 
     return (
         <>
             <h3>{ category }</h3>
-            <h5>{ counter }</h5>
-            <button onClick={ () => setCounter( counter + 1 )}>+1</button>
+
+            <ol>
+                { images.map( ({ id, title }) => (
+                        <li key={ id }> { title } </li>
+                    )
+                )}
+            </ol>
         </>
     )
 }
